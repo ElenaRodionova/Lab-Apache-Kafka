25cbc474c115cd6bcb14142ed3b3ef21f31004ce
ElenaRodionova <93218293+ElenaRodionova@users.noreply.github.com>
2024-03-24 18:26:00 +0300

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import subprocess
 
 subprocess.run(["docker-compose", "up", "-d"]) 
 
-PYTHON_PATH = '/home/new_user/Lab-Apache-Kafka'
+PYTHON_PATH = 'C:\Users\User-PC\AppData\Local\Programs\Python\Python312'
 
 processes = {} 
 processes["1"] = subprocess.Popen([PYTHON_PATH,'./run/data_generation.py']) 
